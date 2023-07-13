dfe35883afeec6911436fc4c29e177effbbf735d
harikagantambotla <124864335+harikagantambotla@users.noreply.github.com>
2023-07-13 13:10:02 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Apna Bana Le - Bhediya",
+        artist: "Arjith Singh",
+        img: "pic1",
+        src: "song1"
+    },
+    {
+        name: "Sun Raha Hai - Aashiqui2",
+        artist: "Shreya Ghoshal",
+        img: "pic2",
+        src: "song2"
+    },
+    {
+        name: "Rait Zara si - Atrangi Re",
+        artist: "A.R. Rahman",
+        img: "pic3",
+        src: "song3"
+    },
+    {
+        name: "Aur Kya Chahiye - Zara Hatke Zera Bachke",
+        artist: "Arjith Singh",
+        img: "pic4",
+        src: "song4"
+    },
+    {
+        name: "Chale Ana - De De Pyaar De",
+        artist: "Armaan Malik",
+        img: "pic5",
+        src: "song5"
+    },
+    {
+        name: "Kala Chashma - Baar Baar Dekho",
+        artist: "Neha Kakkar",
+        img: "pic6",
+        src: "song6"
+    },
+]
